a3816fc088c90e23970f0a1309160d4d660ea94f
Mohamed Anas <a3689211@gmail.com>
2025-01-04 17:49:12 +0530

    nativewind css done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Slot } from 'expo-router';
+import './globals.css'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <Slot />
