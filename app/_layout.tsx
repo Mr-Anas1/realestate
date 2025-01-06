6e3ad2fc830a75c6b341d9d3ea8250f34d088ef0
Mohamed Anas <a3689211@gmail.com>
2025-01-06 18:37:01 +0530

    styles and sign-in page completed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Slot } from 'expo-router';
+import React, { useEffect } from 'react';
+import { Slot, SplashScreen, Stack } from 'expo-router';
 import './globals.css'
+import {useFonts} from "expo-font"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <Slot />
+
+  const [fontsLoaded] = useFonts({
+    "Rubik-Bold": require('../assets/fonts/Rubik-Bold.ttf'),
+    "Rubik-ExtraBold": require('../assets/fonts/Rubik-ExtraBold.ttf'),
+    "Rubik-Light": require('../assets/fonts/Rubik-Light.ttf'),
+    "Rubik-Medium": require('../assets/fonts/Rubik-Medium.ttf'),
+    "Rubik-Regular": require('../assets/fonts/Rubik-Regular.ttf'),
+    "Rubik-SemiBold": require('../assets/fonts/Rubik-SemiBold.ttf'),
+
+  })
+
+  useEffect(()=> {
+    if(fontsLoaded){
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded])
+
+  if(!fontsLoaded) return null;
+
+  return <Stack screenOptions={{headerShown:false }} />
+
+
 }
+
+
 
 
